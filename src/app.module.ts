a622c80707baef4737b90b7be5dfa065945488cf
creed-dev <creed-dev@internet.ru>
2022-12-09 16:12:53 +0700

    refactor app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,36 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigService } from '@nestjs/config';
-import { config } from 'dotenv';
 import { AuthModule } from './auth/auth.module';
-import * as path from 'path';
-
-config();
-
-const configService = new ConfigService();
-
-const entitiesPath: string = path.join(
-  __dirname,
-  'database',
-  'entities',
-  '*.entity.{js,ts}',
-);
+import { DatabaseModule } from './common/database/database.module';
 
 @Module({
-  imports: [
-    UsersModule,
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: configService.get('DB_HOST'),
-      port: configService.get('DB_PORT'),
-      username: configService.get('DB_USER'),
-      password: configService.get('DB_PASSWORD'),
-      database: configService.get('DB_NAME'),
-      entities: [entitiesPath],
-      synchronize: false,
-    }),
-    AuthModule,
-  ],
+  imports: [DatabaseModule, UsersModule, AuthModule],
 })
 export class AppModule {}
---
+++ src/common/database/database.module.ts
@@ -0,0 +1,35 @@
+import { DynamicModule, Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { config } from 'dotenv';
+import * as path from 'path';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+config();
+
+const configService: ConfigService = new ConfigService();
+
+const entitiesPath: string = path.join(
+  __dirname,
+  '..',
+  '..',
+  'database',
+  'entities',
+  '*.entity.{js,ts}',
+);
+
+const typeOrmConfig: DynamicModule = TypeOrmModule.forRoot({
+  type: 'mysql',
+  host: configService.get('DB_HOST'),
+  port: configService.get('DB_PORT'),
+  username: configService.get('DB_USER'),
+  password: configService.get('DB_PASSWORD'),
+  database: configService.get('DB_NAME'),
+  entities: [entitiesPath],
+  synchronize: false,
+});
+
+@Module({
+  imports: [typeOrmConfig],
+  exports: [typeOrmConfig],
+})
+export class DatabaseModule {}
